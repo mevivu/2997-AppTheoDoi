e229948deccb5c9eec1fac4e12f34009fe571a65
minhttran <trcongminh.261002@gmail.com>
2024-11-29 16:22:49 +0700

    Update exercise model & migration

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\ActiveStatus;
 use App\Enums\Exercise\ExerciseType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -12,9 +13,19 @@ class Exercise extends Model
 
     protected $table = 'exercises';
 
-    protected $guarded = [];
+    protected $fillable = [
+        /*Tên bài tập */
+        'name',
+        /*Mô tả*/
+        'description',
+        /*Trạng thái*/
+        'status',
+        /*Loại bài tập*/
+        'exercise_type'
+    ];
 
     protected $casts = [
+        'status' => ActiveStatus::class,
         'exercise_type' => ExerciseType::class
     ];
 }
