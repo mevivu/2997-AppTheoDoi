6591a346f7e6c950315afd76c3c1cd52f1c0e104
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2024-11-29 13:23:20 +0700

    create migrate child

---
+++ database/migrations/2024_11_29_132222_create_children_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Enums\Child\ChildStatus;
+use App\Enums\User\Gender;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('children', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->date('birthday')->nullable();
+            $table->enum('gender', Gender::getValues())->default(Gender::Other->value);
+            $table->enum('status', ChildStatus::getValues())->default(ChildStatus::Active->value);
+
+            $table->timestamps();
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('children');
+    }
+};
