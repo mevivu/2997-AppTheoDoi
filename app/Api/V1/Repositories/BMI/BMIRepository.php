112aa0e28c385ebe1d64b9ff1927b31269dd61f1
tranp6648 <tranp6648@gmail.com>
2024-12-17 14:51:40 +0700

    Update Task

--- app/Api/V1/Repositories/BMI/BMIRepository.php
+++ app/Api/V1/Repositories/BMI/BMIRepository.php
@@ -3,6 +3,7 @@
 namespace App\Api\V1\Repositories\BMI;
 
 use \App\Admin\Repositories\Bmi\BmiRepository as AdminArea;
+use App\Enums\ActiveStatus;
 use App\Models\Bmi;
 
 class BMIRepository extends AdminArea implements BmiRepositoryInterface
@@ -18,7 +19,7 @@ class BMIRepository extends AdminArea implements BmiRepositoryInterface
     public function index($limit = 10, $page = 1)
     {
         // TODO: Implement index() method.
-        return $this->model
+        return $this->model->where('status',ActiveStatus::Active)
             ->paginate($limit, ['*'], 'page', $page);
     }
 }
