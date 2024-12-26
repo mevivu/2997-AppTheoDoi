1134445c294b46cd3d86e4351211d524baaf32fc
tranp6648 <tranp6648@gmail.com>
2024-12-26 10:58:25 +0700

    Update Task

--- app/Api/V1/Providers/RepositoryServiceProvider.php
+++ app/Api/V1/Providers/RepositoryServiceProvider.php
@@ -30,7 +30,7 @@ class RepositoryServiceProvider extends ServiceProvider
         'App\Api\V1\Repositories\Support\SupportRepositoryInterface' => 'App\Api\V1\Repositories\Support\SupportRepository',
         'App\Api\V1\Repositories\SubjectGrade\SubjectGradeRepositoryInterface' => 'App\Api\V1\Repositories\SubjectGrade\SubjectGradeRepository',
         'App\Api\V1\Repositories\Quality\QualityRepositoryInterface' => 'App\Api\V1\Repositories\Quality\QualityRepository',
-        'App\Admin\Repositories\Capability\CapabilityRepositoryInterface' => 'App\Admin\Repositories\Capability\CapabilityRepository',
+        'App\Api\V1\Repositories\Capability\CapabilityRepositoryInterface' => 'App\Api\V1\Repositories\Capability\CapabilityRepository',
     ];
 
     /**
