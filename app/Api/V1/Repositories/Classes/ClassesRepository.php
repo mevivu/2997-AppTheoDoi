38ee9d421eb320b82f996f16dbd90240e0b1f51b
tranp6648 <tranp6648@gmail.com>
2024-12-26 09:30:21 +0700

    Update Task

--- app/Api/V1/Repositories/Classes/ClassesRepository.php
+++ app/Api/V1/Repositories/Classes/ClassesRepository.php
@@ -4,20 +4,13 @@ namespace App\Api\V1\Repositories\Classes;
 
 use \App\Admin\Repositories\Classes\ClassesRepository as AdminArea;
 use App\Enums\ActiveStatus;
-use App\Models\SchoolClass;
 
 class ClassesRepository extends AdminArea implements ClassesRepositoryInterface
 {
     protected $model;
 
-    public function __construct(SchoolClass $note)
-    {
-        $this->model = $note;
-    }
-
     public function findClassesActive()
     {
-        // TODO: Implement findClassesActive() method.
         return $this->model->where('status', ActiveStatus::Active)->get();
     }
 }
