6ccce115eb9f8f07f7bc599f2d5d206b024b31b1
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2024-12-26 10:01:53 +0700

    update model VaccinationSchedule.php

--- app/Models/VaccinationSchedule.php
+++ app/Models/VaccinationSchedule.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use App\Enums\ActiveStatus;
+use App\Enums\Vaccination\VaccinationStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /** Lịch tiêm chủng */
 class VaccinationSchedule extends Model
@@ -14,6 +16,8 @@ class VaccinationSchedule extends Model
     protected $table = 'vaccination_schedules';
 
     protected $fillable = [
+        /** Child ID */
+        'child_id',
         /* Tên phòng khám */
         'name',
         /** Mô tả */
@@ -24,10 +28,18 @@ class VaccinationSchedule extends Model
         'performed_on',
         /* Trạng thái */
         'status',
+        /* Trạng thái tiêm chủng */
+        'vaccination_status'
     ];
     protected $casts = [
         'status' => ActiveStatus::class,
+        'vaccination_status' => VaccinationStatus::class,
         'performed_on' => 'date',
     ];
+
+    public function child(): BelongsTo
+    {
+        return $this->belongsTo(Child::class, 'child_id');
+    }
 
 }
--- database/migrations/2024_12_03_131234_create_vaccination_schedules_table.php
+++ database/migrations/2024_12_03_131234_create_vaccination_schedules_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Enums\ActiveStatus;
+use App\Enums\Vaccination\VaccinationStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -21,6 +22,8 @@ return new class extends Migration
             $table->enum('status', ActiveStatus::getValues())->default(ActiveStatus::Active->value);
             $table->date('performed_on')->nullable();
             $table->text('image')->nullable();
+            $table->enum('vaccination_status', VaccinationStatus::getValues())->default(VaccinationStatus::NotVaccinated->value);
+            $table->foreignId('child_id')->constrained('children')->onDelete('cascade');
             $table->timestamps();
         });
     }
