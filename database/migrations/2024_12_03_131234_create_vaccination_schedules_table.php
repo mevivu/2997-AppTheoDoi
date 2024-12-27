13ae1e434c6f3c43bc76a5c21e8c7e2566b94b23
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2024-12-27 11:53:35 +0700

    update table  vaccination_schedules

--- database/migrations/2024_12_03_131234_create_vaccination_schedules_table.php
+++ database/migrations/2024_12_03_131234_create_vaccination_schedules_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
     {
         Schema::create('vaccination_schedules', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('vaccination_type_id')->nullable();
             $table->string('name');
             $table->text('description')->nullable();
             $table->enum('status', ActiveStatus::getValues())->default(ActiveStatus::Active->value);
@@ -24,8 +25,7 @@ return new class extends Migration
             $table->text('image')->nullable();
             $table->enum('vaccination_status', VaccinationStatus::getValues())->default(VaccinationStatus::NotVaccinated->value);
             $table->foreignId('child_id')->constrained('children')->onDelete('cascade');
-            $table->foreignId('vaccination_type_id')->constrained('vaccination_types')->onDelete('cascade');
-
+            $table->foreign('vaccination_type_id')->references('id')->on('vaccination_types')->onDelete('set null');
             $table->timestamps();
         });
     }
