5639ca94cc45bd7d53aa2e39ca7d6d21aaf5094c
Phong <phongtn.work302@gmail.com>
2024-12-27 16:31:30 +0700

    create file

---
+++ app/Api/V1/Http/Controllers/VaccinationSchedule/VaccinationScheduleController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Api\V1\Http\Controllers\VaccinationSchedule;
+
+use App\Admin\Http\Controllers\Controller;
+
+
+class VaccinationScheduleController extends Controller {}
--- routes/api/v1.php
+++ routes/api/v1.php
@@ -149,6 +149,19 @@ Route::controller(\App\Api\V1\Http\Controllers\ChildEvaluation\ChildEvaluationCo
         Route::put('/', 'update');
     });
 
+// ChildEvaluation
+Route::controller(\App\Api\V1\Http\Controllers\VaccinationSchedule\VaccinationScheduleController::class)
+    ->prefix('/vaccination-schedule')
+    ->as('vaccinationSchedule.')
+    ->group(function () {
+        Route::get('/', 'index');
+        Route::get('/{id}', 'show');
+        Route::post('/', 'store');
+        Route::post('/update', 'update');
+        Route::delete('/{id}', 'delete');
+    });
+
+
 // Exercise
 Route::controller(\App\Api\V1\Http\Controllers\Exercise\ExerciseController::class)
     ->prefix('/exercises')
