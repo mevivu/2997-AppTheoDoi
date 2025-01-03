882bdb37e45b17cb7bde30f836d23a57b2e61a23
TRUONG\Truon <truongdoan120@gmail.com>
2025-01-03 16:04:13 +0700

    Khi tồn tại Kiểm tra EQ và AQ thì không cho tạo thêm

--- app/Admin/Repositories/Quiz/QuizRepositoryInterface.php
+++ app/Admin/Repositories/Quiz/QuizRepositoryInterface.php
@@ -6,5 +6,5 @@ use App\Admin\Repositories\EloquentRepositoryInterface;
 
 interface QuizRepositoryInterface extends EloquentRepositoryInterface
 {
-    //
+    public function existsWithTypes(array $types): bool ;
 }
