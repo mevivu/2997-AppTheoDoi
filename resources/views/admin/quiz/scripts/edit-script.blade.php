ce1152faed2b6c06ba1bb4790bc7d6c2c63b074d
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2025-01-13 13:43:47 +0700

    update ui

--- resources/views/admin/quiz/scripts/edit-script.blade.php
+++ resources/views/admin/quiz/scripts/edit-script.blade.php
@@ -1,14 +1,28 @@
 <script>
     $(document).ready(function() {
         function updateCheckedCount() {
-            var count = $('input[name="question_ids[]"]:checked').length;
+            const count = $('input[name="question_ids[]"]:checked').length;
             $('#checked-count').text(count);
+        }
+
+        function restrictMultipleGroupSelection() {
+            const selectedGroups = {};
+            $('input[name="question_ids[]"]:checked').each(function() {
+                const groupId = $(this).data('group-id');
+                if (selectedGroups[groupId]) {
+                    $(this).prop('checked', false);
+                    alert('Chỉ được chọn một câu hỏi trong mỗi nhóm.');
+                } else {
+                    selectedGroups[groupId] = true;
+                }
+            });
+            updateCheckedCount();
         }
 
         updateCheckedCount();
 
         $(document).on('change', 'input[name="question_ids[]"]', function() {
-            updateCheckedCount();
+            restrictMultipleGroupSelection();
         });
     });
 </script>
