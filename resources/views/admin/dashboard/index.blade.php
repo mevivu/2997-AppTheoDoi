bb9bb8e9176c1aa4741ee3e1d94db1b0dad56d40
TRUONG\Truon <truongdoan120@gmail.com>
2025-01-15 11:52:43 +0700

    sui UI

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,17 +1,7 @@
 @extends('admin.layouts.master')
 
 @section('content')
-    <div class="page-header d-print-none">
-        <div class="container-xl">
-            <div class="row g-2 align-items-center">
-                <div class="col">
-                    <h2 class="page-title">
-                        {{ __('Dashboard') }}
-                    </h2>
-                </div>
-            </div>
-        </div>
-    </div>
+
     <div class="page-body">
         <div class="container-xl">
             <div class="row">
