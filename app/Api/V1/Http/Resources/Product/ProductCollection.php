f8de65b0c4a31cb4b4c1af1e9b5ff46792b650f7
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2025-03-05 09:52:17 +0700

    update vaccination schedule

---
+++ app/Api/V1/Http/Resources/Product/ProductCollection.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Api\V1\Http\Resources\Product;
+
+use App\Api\V1\Http\Resources\Rating\RatingResource;
+use App\Api\V1\Http\Resources\RatingPQ\RatingPQResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'products' => $this->collection->map(function ($item) {
+                return new ProductResource($item);
+            }),
+            'links' => [
+                'first' => $this->url(1),
+                'last' => $this->url($this->lastPage()),
+                'prev' => $this->previousPageUrl(),
+                'next' => $this->nextPageUrl(),
+            ],
+            'meta' => [
+                'current_page' => $this->currentPage(),
+                'from' => $this->firstItem(),
+                'to' => $this->lastItem(),
+                'limit' => $this->perPage(),
+                'total' => $this->total(),
+                'count' => $this->count(),
+                'total_pages' => $this->lastPage(),
+            ],
+        ];
+    }
+
+}
