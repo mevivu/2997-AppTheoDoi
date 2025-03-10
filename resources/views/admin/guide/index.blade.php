f7c7c4df75a95c8f9c33ab8c04cce2a816de3a08
TRUONG\Truon <truongdoan120@gmail.com>
2025-03-10 11:34:44 +0700

    sửa lỗi UI

--- resources/views/admin/guide/index.blade.php
+++ resources/views/admin/guide/index.blade.php
@@ -8,7 +8,7 @@
         <div class="container-xl">
             <div class="card custom-shadow">
                 <div class="card-header justify-content-between">
-                    <h2 class="mb-0">@lang('list')</h2>
+                    <h2 class="mb-0">@lang('Danh sách Hướng Dẫn')</h2>g
                     <x-link :href="route('admin.guide.create')" class="btn btn-primary">
                         <i class="ti ti-plus"></i>
                         <span class="ms-1">@lang('add')</span>
