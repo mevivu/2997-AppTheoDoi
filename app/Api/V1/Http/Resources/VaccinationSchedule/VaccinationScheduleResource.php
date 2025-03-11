38b6717f9df8896a078c1fe6f28230d555328255
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2025-03-11 14:16:51 +0700

    update vaccination

--- app/Api/V1/Http/Resources/VaccinationSchedule/VaccinationScheduleResource.php
+++ app/Api/V1/Http/Resources/VaccinationSchedule/VaccinationScheduleResource.php
@@ -23,7 +23,7 @@ class VaccinationScheduleResource extends JsonResource
             'description' => $this->description,
             'vaccination_status' => $this->vaccination_status,
             'performed_on' => format_date($this->performed_on),
-            'image' => $this->image,
+            'image' => formatImageUrl($this->image),
             'vaccination_types' => [
                 'name' => $this->vaccinationType->name ?? null,
             ],
