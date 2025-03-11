f5057e0a71ec435885474463a030d2054a4fe6a5
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2025-03-11 10:32:24 +0700

    update PregnancyResource

--- app/Api/V1/Http/Requests/Pregnancy/PregnancyUpdateRequest.php
+++ app/Api/V1/Http/Requests/Pregnancy/PregnancyUpdateRequest.php
@@ -16,8 +16,6 @@ class PregnancyUpdateRequest extends BaseRequest
         return [
             'id' => 'required|integer|exists:pregnancies,id',
             'child_id' => ['required', 'integer', new ValidChild()],
-            'start_date' => 'required|date_format:Y-m-d',
-            'end_date' => 'required|date_format:Y-m-d',
             'week' => 'sometimes|integer|min:1',
             'weight' => 'sometimes|numeric|min:0',
             'length' => 'sometimes|integer|min:0',
