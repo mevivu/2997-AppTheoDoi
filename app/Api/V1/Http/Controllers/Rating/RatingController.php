8b2c84c72a7a612d0d6796149c29821d6a87afa1
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2025-03-14 14:28:42 +0700

    update api rating

--- app/Api/V1/Http/Controllers/Rating/RatingController.php
+++ app/Api/V1/Http/Controllers/Rating/RatingController.php
@@ -136,6 +136,7 @@ class RatingController extends Controller
      * @authenticated
      * @bodyParam child_id int required ID của trẻ mà đánh giá được tạo cho. Example: 1
      * @bodyParam type string required Loại câu hỏi đang được đánh giá. Example: iq
+     * @bodyParam tag string required Bố, Mẹ,Khác. Example: Bố
      * @bodyParam answers array required Một mảng các câu trả lời với ID câu hỏi và ID câu trả lời.
      * @bodyParam answers[].question_id int required ID của câu hỏi đang được trả lời.
      * @bodyParam answers[].answer_id int required ID của câu trả lời được cung cấp.
