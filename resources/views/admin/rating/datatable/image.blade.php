a10727c5dc5cbe00e5684068902fcc329b6b6105
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2025-05-05 16:58:54 +0700

    update package service

--- resources/views/admin/rating/datatable/image.blade.php
+++ resources/views/admin/rating/datatable/image.blade.php
@@ -1,4 +1,8 @@
-<img src="{{ asset($image) }}"
-     class="rounded mx-auto d-block"
-     style="width: 100px"
-     alt="{{ __('Ảnh bài viết') }}">
+@if(!empty($image))
+    <img src="{{ asset($image) }}"
+         class="rounded mx-auto d-block"
+         style="width: 100px"
+         alt="{{ __('Ảnh bài viết') }}">
+@else
+    <span>N/A</span>
+@endif
