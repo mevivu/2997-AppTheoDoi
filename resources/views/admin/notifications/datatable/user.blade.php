5b7dabdaaec1c685b3d4ce31b8688d9c013522aa
DOng <khanhdong1702pro@gmail.com>
2025-05-05 17:09:52 +0700

    update

--- resources/views/admin/notifications/datatable/user.blade.php
+++ resources/views/admin/notifications/datatable/user.blade.php
@@ -1,5 +1,5 @@
 @php
-    $test = App\Models\User::find($user_id);
+    $test = App\Models\User::find($user_id_attribute);
 @endphp
 @if ($test)
     <x-link target="_blank" :href="route('admin.user.edit', $test->id)" :title="$test->fullname" class="text-decoration-none" />
