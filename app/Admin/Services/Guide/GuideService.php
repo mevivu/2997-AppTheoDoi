d6959c2e3bf82c8ba69ae45e46e965d115653a79
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2025-05-16 11:11:01 +0700

    update

--- app/Admin/Services/Guide/GuideService.php
+++ app/Admin/Services/Guide/GuideService.php
@@ -100,7 +100,30 @@ class GuideService implements GuideServiceInterface
 
     public function updateSteps(object $guide, array $steps): void
     {
-        $guide->steps()->delete();
-        $this->storeSteps($guide, $steps);
+        $existingSteps = $guide->steps()->get()->keyBy('id');
+        $processedIds = [];
+
+        foreach ($steps as $step) {
+            if (!empty($step['id']) && $existingSteps->has($step['id'])) {
+                // Update existing step
+                $existingSteps[$step['id']]->update([
+                    'title'       => $step['title'],
+                    'description' => $step['description'] ?? null,
+                    'order'       => $step['order'],
+                ]);
+                $processedIds[] = $step['id'];
+            } else {
+                // Create new step
+                $newStep = $guide->steps()->create([
+                    'title'       => $step['title'],
+                    'description' => $step['description'] ?? null,
+                    'order'       => $step['order'],
+                ]);
+                $processedIds[] = $newStep->id;
+            }
+        }
+
+        // Delete steps that are no longer present
+        $guide->steps()->whereNotIn('id', $processedIds)->delete();
     }
 }
