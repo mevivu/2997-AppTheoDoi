30a46fbd5833d76db1e61999091e68c2828d89e1
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2025-05-22 17:09:39 +0700

    update rating pq service

--- app/Api/V1/Http/Resources/RatingPQ/RatingPQResource.php
+++ app/Api/V1/Http/Resources/RatingPQ/RatingPQResource.php
@@ -33,6 +33,7 @@ class RatingPQResource extends JsonResource
             'strength' => $this->strength,
             'endurance' => $this->endurance,
             'bmi' => $this->bmi,
+            'score' => $this->score,
             'bmi_result' => $this->bmi_result,
             'height_result' => $this->height_result,
             'height_change' => round($this->height_change, 2),
