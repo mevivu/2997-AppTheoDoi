cf609bb1531e77fea3a044a2768dea3a2bd54bcd
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2026-07-19 19:41:05 +0700

    feat: add edit action button to app version datatable

--- resources/views/admin/app_versions/datatable/action.blade.php
+++ resources/views/admin/app_versions/datatable/action.blade.php
@@ -1,5 +1,3 @@
-@can('mevivuDev')
-    <a href="{{ route('admin.app-version.edit', $id) }}" class="btn btn-icon btn-outline-primary" title="{{ __('Sửa') }}">
-        <i class="ti ti-edit"></i>
-    </a>
-@endcan
+<a href="{{ route('admin.app-version.edit', $id) }}" class="btn btn-icon btn-outline-primary" title="{{ __('Sửa') }}">
+    <i class="ti ti-edit"></i>
+</a>
