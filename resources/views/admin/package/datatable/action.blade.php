928ddf1a8648fcba70e2e36bf9fc5e4c6b97e094
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2026-08-18 22:03:23 +0700

    feat: add action button components for package and role datatables

--- resources/views/admin/package/datatable/action.blade.php
+++ resources/views/admin/package/datatable/action.blade.php
@@ -1,4 +1,3 @@
 <x-admin.datatable.action-group>
     <x-admin.datatable.action-edit :href="route('admin.package.edit', $id)" />
-    <x-admin.datatable.action-delete :route="route('admin.package.delete', $id)" />
 </x-admin.datatable.action-group>
--- resources/views/admin/role/datatable/action.blade.php
+++ resources/views/admin/role/datatable/action.blade.php
@@ -1,4 +1,4 @@
 <x-admin.datatable.action-group>
     <x-admin.datatable.action-edit :href="route('admin.role.edit', $id)" />
-    <x-admin.datatable.action-delete :route="route('admin.role.delete', $id)" />
+    {{-- <x-admin.datatable.action-delete :route="route('admin.role.delete', $id)" /> --}}
 </x-admin.datatable.action-group>
