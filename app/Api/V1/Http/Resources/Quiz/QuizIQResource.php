329389728d5a9e3fe1706c1b9c33d5971894320f
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2026-09-19 14:28:33 +0700

    feat: add QuizIQResource to transform quiz IQ data and memo games

--- app/Api/V1/Http/Resources/Quiz/QuizIQResource.php
+++ app/Api/V1/Http/Resources/Quiz/QuizIQResource.php
@@ -14,10 +14,11 @@ class QuizIQResource extends ResourceCollection
             $memoGames = MemoGameBuilderService::buildRounds((int) ($quiz->age ?? 1));
             $hasGame = !empty($memoGames);
             $gamePlays = count($memoGames);
-            $gameDuration = 0;
+            $roundDuration = 0;
             if ($hasGame && !empty($memoGames[0]['age_config']['total_duration'])) {
-                $gameDuration = (int) $memoGames[0]['age_config']['total_duration'];
+                $roundDuration = (int) $memoGames[0]['age_config']['total_duration'];
             }
+            $totalGameDuration = $roundDuration * $gamePlays;
 
             return [
                 'id' => $quiz->id,
@@ -25,7 +26,9 @@ class QuizIQResource extends ResourceCollection
                 'type' => $quiz->type,
                 'has_game' => $hasGame,
                 'game_plays' => $gamePlays,
-                'game_duration' => $gameDuration,
+                'game_duration' => $roundDuration,
+                'round_duration' => $roundDuration,
+                'total_game_duration' => $totalGameDuration,
                 'memo_games' => $memoGames,
                 'questions' => QuestionResource::collection($quiz->questions),
             ];
